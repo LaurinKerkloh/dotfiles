bcebc734bc7f97f86b9f55ccacbc99ad5e679c54
Laurin Kerkloh <laurin@kerk-loh.de>
2025-05-22 15:30:34 +0200

    remove tmux sessionizer keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,6 +22,3 @@ vim.keymap.set("v", "<M-k>", ":m '<-2<cr>gv=gv")
 -- Navigate quickfix
 vim.keymap.set("n", "<C-M-j>", "<cmd>cnext<cr>zz")
 vim.keymap.set("n", "<C-M-k>", "<cmd>cprev<cr>zz")
-
--- tmux-sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new-window tmux-sessionizer<cr>")
